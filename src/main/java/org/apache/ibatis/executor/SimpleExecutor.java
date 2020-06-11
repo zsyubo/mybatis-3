5b5ad0d22ebb36da2d5ea97eb4a0e38ee023ddcc
huyuanfu <huyuanfu@duizhuang.com>
2020-06-11 21:02:19 +0800

    源码学习

--- src/main/java/org/apache/ibatis/executor/SimpleExecutor.java
+++ src/main/java/org/apache/ibatis/executor/SimpleExecutor.java
@@ -15,12 +15,6 @@
  */
 package org.apache.ibatis.executor;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Collections;
-import java.util.List;
-
 import org.apache.ibatis.cursor.Cursor;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.logging.Log;
@@ -30,6 +24,12 @@ import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.transaction.Transaction;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Clinton Begin
@@ -84,6 +84,7 @@ public class SimpleExecutor extends BaseExecutor {
     Statement stmt;
     Connection connection = getConnection(statementLog);
     stmt = handler.prepare(connection, transaction.getTimeout());
+    // 处理查询的结果
     handler.parameterize(stmt);
     return stmt;
   }
